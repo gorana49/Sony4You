667e39917948e38a551d88695f3e9799ea80a109
mihajlo202 <mihajlo202@hotmail.com>
2021-05-21 15:24:55 +0200

    redis controller, service, startup set up

---
+++ back/back/IService/IRedisService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back
+{
+    public interface IRedisService
+    {
+        Task<string> GetCacheValueAsync(string key);
+        Task SetCacheValueAsync(string key, string value);
+    }
+}
--- back/back/Startup.cs
+++ back/back/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Neo4j.Driver;
+using StackExchange.Redis;
 
 namespace back
 {
@@ -20,14 +21,20 @@ namespace back
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddSingleton<IConnectionMultiplexer>(x =>
+            ConnectionMultiplexer.Connect(Configuration.GetValue<string>("ConnectionStrings:redisServerUrl")));
+          
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "back", Version = "v1" });
             });
-            services.AddStackExchangeRedisCache(options => options.Configuration = this.Configuration.GetConnectionString("redisServerUrl"));
+            //services.AddStackExchangeRedisCache(options => options.Configuration = this.Configuration.GetConnectionString("redisServerUrl"));
+            services.AddSingleton<IRedisService, RedisService>();
             services.AddSingleton(GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "adminadmin")));
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
