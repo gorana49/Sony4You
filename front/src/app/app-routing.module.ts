fb5adeb596c2d3e8f2a8d65c83292e1912ba3d9c
mihajlo202 <mihajlo202@hotmail.com>
2021-06-25 16:09:32 +0200

    new front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,34 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MainPageComponent } from './components/main-page/main-page.component';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {path: '', redirectTo: '/mainPage', pathMatch: 'full'},
-  {path: 'mainPage', component: MainPageComponent},
-  // {
-  //   path: 'renterer',
-  //   component: EmployerComponent,
-  //   canActivate:[AuthRoleGuard],
-  //   children: [
-  //     {path: '', component: EmployerProfileComponent },
-  //     {path: 'profil', component: EmployerProfileComponent},
-  //     {path: 'main', component: SearchWorkersComponent}
-  //   ],
-  //   data: { role: 'renterer'}
-  // },
-  // {
-  //   path: 'rentee',
-  //   component: WorkerComponent,
-  //   children: [
-  //     {path: '', component: WorkerProfileComponent },
-  //     {path: 'profil', component: WorkerProfileComponent},
-  //     {path: 'main', component: SearchJobsComponent}
-  //   ],
-  //   canActivate:[AuthRoleGuard],
-  //   data: { role: 'rentee'}
-  // },
-  {path: '**', redirectTo: 'mainPage', pathMatch: 'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,34 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MainPageComponent } from './components/main-page/main-page.component';
-import { AuthRoleGuard } from './services/auth-role.guard';
-import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RegisterComponent,
-    LoginComponent,
-    MainPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers:[
-    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-    JwtHelperService,
-    AuthRoleGuard
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
