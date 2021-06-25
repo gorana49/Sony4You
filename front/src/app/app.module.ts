3a4402577e90831f7e413c2fd37ee0a7ef0a690f
mihajlo202 <mihajlo202@hotmail.com>
2021-06-25 17:03:47 +0200

    main page, register, log in init

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
