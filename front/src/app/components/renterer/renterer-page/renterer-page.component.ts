e2c5a340995ed49b00c8da3c2b26dddde41b65cb
mihajlo202 <mihajlo202@hotmail.com>
2021-07-01 17:12:57 +0200

    front-renterer-service

--- front/src/app/components/renterer/renterer-page/renterer-page.component.ts
+++ front/src/app/components/renterer/renterer-page/renterer-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Renterer } from 'src/app/models/Renterer';
+import { RentererService } from 'src/app/services/renterer.service';
 
 @Component({
   selector: 'app-renterer-page',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./renterer-page.component.css']
 })
 export class RentererPageComponent implements OnInit {
-
-  constructor() { }
+  renterer: Renterer;
+  constructor(private router:Router,
+              private rentererService:RentererService) { 
+                this.renterer = new Renterer("","","","","","","","");
+              }
 
   ngOnInit(): void {
+    var user:Renterer = JSON.parse(localStorage.getItem("user"));
+    if(user!= null){
+      this.rentererService.getEmployerByUsername(user.username)
+      .subscribe(value=>{
+        if(value!=undefined){
+          this.renterer = value;
+          console.log(value)
+        }
+        else{
+          this.renterer = new Renterer("","","","","","","","");
+        }
+      })
+    }
+    else{
+      this.renterer = new Renterer("","","","","","","","");
+    }
   }
 
+  navigateTo(path) {
+    this.router.navigate([`./renterer/${path}`]);
+  }
 }
